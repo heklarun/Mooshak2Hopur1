2abd39890b861d104c8f552c2baa0ec9ad48b763
bryndis15 <verklegtnamskei1>
2016-05-06 18:12:32 +0000

    lettvægar breytingarrrrr :D

--- Mooshak2/Models/TeacherInputsResults.cs
+++ Mooshak2/Models/TeacherInputsResults.cs
@@ -7,6 +7,13 @@ namespace Mooshak2.Models
 {
     public class TeacherInputsResults
     {
+        SubProjects h;
+        public int userID { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
 
     }
 }
