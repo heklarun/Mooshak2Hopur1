48c7aa90dae0319f7d6f5f152d2ff4112f955d26
Matthildur <matthildur15@ru.is>
2016-05-12 15:36:14 +0000

    Einingaprófanir

---
+++ UnitTestProject1/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using SozialWeb.test.services;
+//using SozialWeb.services;
+
+namespace UnitTestProject1.Controllers
+{
+    [TestClass]
+    public class UserService
+    {
+        private UserService _service;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var mockDB = new MockDatabase();
+            _service = new UserService();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrrange:
+            //Act:
+            //Assert:
+        }
+    }
+}
